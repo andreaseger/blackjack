cec283bf7738697ddc42bfe245f86632575dfa54
Eger Andreas <andreas.eger.reg@googlemail.com>
2011-01-14 10:08:38 -0800

    first specs
    Signed-off-by: Eger Andreas <andreas.eger.reg@googlemail.com>

---
+++ spec/blackjack/game_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+module Blackjack
+  describe Game do
+    describe '#start' do
+      it "should show a welcome message" do
+        pending
+      end
+      it "should promt for the first card" do
+        pending
+      end
+    end
+  end
+end
